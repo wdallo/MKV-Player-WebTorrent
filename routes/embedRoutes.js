10836af648ec75302cc9393ff3f5536df3d7b5ac
wdallo <web.dalyzz@gmail.com>
2026-03-03 14:44:16 +0200

    fix

--- routes/embedRoutes.js
+++ routes/embedRoutes.js
@@ -4,12 +4,7 @@ const router = express.Router();
 router.get("/embed", (req, res) => {
   const url = req.query.url || "";
 
-  // Check if it's a torrent site browsing request
-  if (url && (url.startsWith("http://") || url.startsWith("https://"))) {
-    res.render("browser", { url });
-  } else {
-    res.render("embed", { url });
-  }
+  res.render("embed", { url });
 });
 
 export default router;
